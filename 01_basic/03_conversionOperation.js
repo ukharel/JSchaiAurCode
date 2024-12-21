32bddfb66902f3d987a30f2e1ae3a557ff24f058
ukharel <146238225+ukharel@users.noreply.github.com>
2024-12-21 10:37:01 +0000

    conversion

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -14,4 +14,29 @@ let booleanIsLoggedIn = Boolean(loggedIn)
 console.log(booleanIsLoggedIn)
 */
 let date = new Date('2024-12-25T02:34:00')
-console.log (Number(date))
+//console.log (Number(date))
+// ****************OPERATIONS****************
+
+let value = 4
+let NegValue = -value
+//console.log(NegValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%3)
+
+str0 = 'Hello';
+str1 = ' Ujjwal';
+str2 = str0 + str1
+console.log(str2);
+console.log('1'+2+8);
+console.log(1+'2');
+
+console.log(2+6+'6')
+
+console.log(+true);
+console.log(+"")
+
+
