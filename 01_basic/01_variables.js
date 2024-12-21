e90dab22fa5a46aba5f7e3aca96fa7209efa5820
ukharel <146238225+ukharel@users.noreply.github.com>
2024-12-21 07:38:18 +0000

    datatypes

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,12 @@
+const accountId= 2233
+let accountEmail = 'ujjwal@yahoo.com'
+var accountPassword = '123345'
+accountCity = 'Jhapa'
+
+//accountId = 34// not allowed
+
+/*
+Don't use var because of the issue of block scope and functional scope.
+*/
+
+console.log(accountId);
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"
+//alert(3 + 3) //not allowed because we are using node not browser.
+
+console.log('Ujjwal')
+
+let name = 'Ujjwal';
+let age = 32;
+let isLoggedIn = false
+
+
+//number => range is around 2 to the power 52
+//bigint
+//string
+//boolean => gives either True or False
+//symbol => uniqueness in data
+//null => standalone value
+// undefined => when there is no value.
+
+//object
+console.log(typeof "Ujjwal")
+
