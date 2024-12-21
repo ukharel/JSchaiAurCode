64d111f3f7e57235815de1bf2cccca52bd6763a6
ukharel <146238225+ukharel@users.noreply.github.com>
2024-12-21 09:23:43 +0000

    conversion of data type

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,17 @@
+//let score = "40";
+//console.log(typeof (score));
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+
+// "33" =33
+// "33anc" => NaN
+// true => 1  false => 0
+// null => 0
+// undefined => NaN
+/*let loggedIn = "Ujjwal"
+let booleanIsLoggedIn = Boolean(loggedIn)
+console.log(booleanIsLoggedIn)
+*/
+let date = new Date('2024-12-25T02:34:00')
+console.log (Number(date))
