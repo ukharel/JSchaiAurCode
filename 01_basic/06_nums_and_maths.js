bb3186f85079dfd295eb0bead9c585f4474c65a8
ukharel <146238225+ukharel@users.noreply.github.com>
2024-12-22 03:10:31 +0000

    nums and maths

---
+++ 01_basic/06_nums_and_maths.js
@@ -0,0 +1,25 @@
+const score = 100
+let newnumber = new Number(100)
+// console.log(newnumber)
+// console.log(newnumber.toString())
+// console.log(newnumber.toFixed(3))
+
+const number = 1234.1234
+// console.log(number.toPrecision(5))
+
+const localString = 10000000
+// console.log(localString.toLocaleString('en-IN'))
+
+//++++++++++++++++ Maths +++++++++++++++++
+// console.log(Math.floor(12.22))
+// console.log(Math.ceil(5.4))
+// console.log(Math.min(3,2,6,7))
+// console.log(Math.max(9,3,2,7,8))
+
+
+console.log(Math.random())
+console.log((Math.random())* 10)+1
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()* (max-min +1) + min))
+
