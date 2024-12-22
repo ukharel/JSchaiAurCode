e8018291b74b90ca2cd6bd5c5159351748d97ba4
ukharel <146238225+ukharel@users.noreply.github.com>
2024-12-22 05:22:48 +0000

    date and time

---
+++ 01_basic/07_DateinJS.js
@@ -0,0 +1,16 @@
+let MyDate = new Date();
+console.log(MyDate.toString())
+console.log(MyDate.toDateString())
+console.log(MyDate.toLocaleString())
+
+// let CreatedDate = new Date(2023,1,3)
+// let CreatedDate = new Date(2023,1,3,3,4)
+ let CreatedDate = new Date("2021-05-12")
+
+// console.log(CreatedDate.toLocaleString())
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp)
+console.log(Math.floor(Date.now()/1000))
+
+
