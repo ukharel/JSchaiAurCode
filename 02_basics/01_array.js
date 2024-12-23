de42b0966c11656d972239ef6fb728690d2595d9
ukharel <146238225+ukharel@users.noreply.github.com>
2024-12-23 04:48:25 +0000

    array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,35 @@
+const myArray = [1,2,3,4,5]
+const myArray1 = new Array('hello','how are you', '1','2')
+const Array1 = ['Shaktiman','Naagraj']
+
+// console.log(Array1[1])
+
+//Array methods
+
+// myArray.push(9)
+// myArray.push(7)
+// myArray.pop()
+// myArray.pop()
+
+
+// myArray.unshift(2)
+// myArray.shift(4)
+
+// console.log(myArray.includes(9));
+// console.log(myArray.indexOf(9));
+//  const newArray = myArray.join() // change elements into string
+//  console.log(myArray)
+//  console.log(newArray)
+
+console.log('A', myArray)
+const myn1 = myArray.slice(1,3)
+console.log(myn1)
+console.log('B', myArray)
+const myn2 = myArray.splice(1,3)
+console.log('C',myArray)
+console.log(myn2)
+
+
+
+
+
