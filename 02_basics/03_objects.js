b11c7d2dcb75211a0098ad903e4cdf01bab87724
ukharel <146238225+ukharel@users.noreply.github.com>
2024-12-24 07:37:18 +0000

    objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,30 @@
+// singleton
+//Objects literals
+
+const mySym =Symbol('key1')
+
+const Jsuser = {
+    name:'Ujjwal',"fullname": "Ujjwal Kharel", [mySym]:"mykey1",
+    age: 19,
+    location: "jaipur",
+    mail:"ujjwalkharelpandit17mail.com",
+    isLoggedIn : false,
+    LastLogInDays : ['Monday','Friday'],
+
+
+}
+
+console.log(Jsuser['age'])
+console.log(Jsuser.mail)
+console.log(Jsuser["fullname"])
+console.log(Jsuser[mySym])
+
+Jsuser.mail = "kharelujjwal@gmail.com"
+
+// Object.freeze(Jsuser)
+Jsuser.mail = "kharelujjwal123@gmail.com"
+console.log(Jsuser)
+Jsuser.greeting = function(){
+    console.log('Hellow Js User World')
+}
+console.log(Jsuser.greeting())
