1a27771f7d9283313fcca9fb27f6f2634295db86
ukharel <146238225+ukharel@users.noreply.github.com>
2024-12-27 04:18:28 +0000

    function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -8,7 +8,7 @@ function addTwoNumbers(num1,num2){
     
 }
 const result = addTwoNumbers(2,3)
-console.log(result)
+// console.log(result)
 
 function LoginUserMessege(username){
 
@@ -19,5 +19,30 @@ function LoginUserMessege(username){
     return `${username} just logged in.`
 
 }
-console.log(LoginUserMessege('Ujjwal'))
+// console.log(LoginUserMessege('Ujjwal'))
 
+
+
+function CalculateCartPrice(val1,val2, ...num1){
+    return num1
+}
+// console.log(CalculateCartPrice(200,122,134,154,354)
+const user = {
+    name:'Ujjwal',
+    price: 123
+}
+
+function handleObject (user){
+    console.log(`Username is ${user.name} and price is ${user.price}`)  
+}
+handleObject({
+    username:"Ujjwal",
+    price: 200
+})
+
+const myArray = [100,200,300,400]
+
+function getSecondValue (getArray){
+    return getArray[1]
+}
+console.log(getSecondValue(myArray))
