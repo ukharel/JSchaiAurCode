9693c078d11b636f9a119f2f113f98fb172cf658
ukharel <146238225+ukharel@users.noreply.github.com>
2024-12-28 06:52:05 +0000

    high order

---
+++ 05_iterations/highorderarray.js
@@ -0,0 +1,13 @@
+const programming = ['js','python','c++','ruby']
+// programming.forEach( function (val){
+//     console.log(val)
+// })
+
+programming.forEach( (item) => {
+    // console.log(item)
+})
+
+function printMe (item){
+    console.log(item)
+}
+// programming.forEach(printMe)
