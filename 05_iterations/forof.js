005f4ec60c2ff14e6900c9530884b467cb77964e
ukharel <146238225+ukharel@users.noreply.github.com>
2024-12-28 06:51:57 +0000

    for of

--- 05_iterations/forof.js
+++ 05_iterations/forof.js
@@ -4,6 +4,55 @@
 
 const arr = [1,2,3,4,5]
 for (const num of arr) {
-    console.log(num)
+   // console.log(num)
     
+}
+
+const greeting = " Hello world"
+for (const greet of greeting) {
+    if (greet == " "){
+        continue
+    }
+
+    // console.log(`Each character is ${greet}`)
+    
+}
+
+const map1 = new Map()
+map1.set('Province1','Koshi')
+map1.set('Province2','Bagmati')
+map1.set('Province3','Pokhara')
+
+// console.log(map1)
+
+for (const [key,value] of map1) {
+    // console.log(key + ':=' + value)
+    
+}
+
+// const obj = {
+//     "game1":'Hokey',
+//     "game2": 'football'
+// }
+
+// for (const [key,value] of obj){
+//     // console.log(key + ':=' + value)
+   
+// }
+
+const myObj = {
+    name : "Ujjwal",
+    age:23,
+    class: "bachelor"
+}
+
+for (const key in myObj) {
+    // console.log(key, myObj[key])
+    
+}
+
+const myArray = [2,3,4,5,6]
+
+for (const key in myArray) {
+    console.log(key, myArray[key])
 }
