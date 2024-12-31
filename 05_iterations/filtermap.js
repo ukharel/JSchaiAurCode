e3177fd6932b7f71d922211ca636af87ac8fbd80
ukharel <146238225+ukharel@users.noreply.github.com>
2024-12-31 08:21:01 +0000

    filter

--- 05_iterations/filtermap.js
+++ 05_iterations/filtermap.js
@@ -13,6 +13,6 @@ let newNumber = Nums.filter( (num) => {
 // console.log(newNumber)
 
 const number = [1,2,3,4,5,6,7,8,9,10]
-const newnumber= number.map( (num) => num + 10 )
+const newnumber= number.map( (num) => num * 10 ).map( (num) => num + 1 ).filter( (num) => num >=30 )
 console.log(newnumber)
 
